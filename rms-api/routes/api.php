dabb8b6688688631749d74ab77414337c3e474bd
alexangria1992 <alex040892@gmail.com>
2022-09-18 20:29:29 +1200

    Routing and JWT Authentication

--- rms-api/routes/api.php
+++ rms-api/routes/api.php
@@ -4,4 +4,13 @@ use App\Http\Controllers\auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/auth/login', [AuthController::class, 'login']);
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
